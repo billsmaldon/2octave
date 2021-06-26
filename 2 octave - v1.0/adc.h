937184aa8e0f7a6feb377fda45b2b72ecc3daae8
billsmaldon <82852742+billsmaldon@users.noreply.github.com>
2021-06-25 23:02:58 -0600

    Add files via upload

--- 2 octave - v1.0/adc.h
+++ 2 octave - v1.0/adc.h
@@ -1,17 +1,17 @@
-/* This file is part of Foobar.
+/* This file is part of 2octave.
 
-    Foobar is free software: you can redistribute it and/or modify
+    2octave is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    2octave is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+    along with 2octave.  If not, see <https://www.gnu.org/licenses/>.
 
 */
 
